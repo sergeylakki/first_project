d401201bec48fd98cb42b52854fea7d7cfee2826
Sergey <sergeylakki@mail.ru>
2024-12-15 14:03:23 +0300

    create line

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import tkinter as tk
+
+win = tk.Tk()
+canvas = tk.Canvas(win, bg='black', height=400, width=400)
+for i in range(50,400,50):
+    canvas.create_line((0, i), (600,i), fill='white')
+    canvas.create_line((i, 0), (i, 600), fill='white')
+canvas.pack()
+tk.mainloop()
